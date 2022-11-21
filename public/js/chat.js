6f78462831ce4f227b1d3b0d325aba4622a01987
Daniel Mateu <danielmateu86@gmail.com>
2022-11-21 15:44:07 +0100

    maquetando html

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,6 +5,14 @@ const url = (window.location.hostname.includes('localhost'))
 let usuario = null;
 let socket  = null;
 
+//Referencias HTMLº
+
+const txtUid = document.querySelector('#txtUid');
+const txtMensaje = document.querySelector('#txtMensaje');
+const ulUsuarios = document.querySelector('#ulUsuarios');
+const ulMensajes = document.querySelector('#ulMensajes');
+const btnSalir = document.querySelector('#btnSalir');
+
 //Validar token del localStorage
 const validarJWT = async() => {
     const token = localStorage.getItem('token') || '';
@@ -30,11 +38,31 @@ const validarJWT = async() => {
 
 const conectarSocket = async() => {
 
-    const socket = io({
+    socket = io({
         'extraHeaders': {
             'x-token': localStorage.getItem('token'),
         }
     });
+
+    socket.on('connect', () => {
+        console.log('Sockets online');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Sockets offline');
+    });
+
+    socket.on('recibir-mensajes', () => {
+        //TODO
+    })
+
+    socket.on('usuarios-activos', () => {
+        //TODO
+    })
+
+    socket.on('mensaje-privado', () => {
+        //TODO
+    })
 }
 
 const main = async() => {
