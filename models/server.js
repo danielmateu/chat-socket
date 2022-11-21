c944a7284faf4d431a13d3c580ca580708d53b5d
Daniel Mateu <danielmateu86@gmail.com>
2022-11-21 12:23:00 +0100

    instalamos socket.io

--- models/server.js
+++ models/server.js
@@ -7,8 +7,10 @@ const { dbConnection } = require('../database/config');
 class Server {
 
     constructor() {
-        this.app  = express();
-        this.port = process.env.PORT;
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io     = require('socket.io')(this.server);
 
         this.paths = {
             auth        : '/api/auth',
