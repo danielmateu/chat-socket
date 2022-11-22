ff67fbdc509b0ff14d3052a18ac0b6917d59244e
Daniel Mateu <danielmateu86@gmail.com>
2022-11-22 10:19:43 +0100

    Mensajes privados

--- public/js/chat.js
+++ public/js/chat.js
@@ -58,8 +58,8 @@ const conectarSocket = async() => {
 
     socket.on('usuarios-activos', dibujarUsuarios)
 
-    socket.on('mensaje-privado', () => {
-        //TODO
+    socket.on('mensaje-privado', (payload) => {
+        console.log('Privado:', payload);
     })
 }
 
